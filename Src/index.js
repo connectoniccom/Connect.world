1fc91e56846c67b10e1756bd5c0c6452dded5307
connectoniccom <Connectonic.com@gmail.com>
2025-06-06 20:59:43 +0300

    Update index.js

--- Src/index.js
+++ Src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '/App.css';
+import App from './App';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+);
