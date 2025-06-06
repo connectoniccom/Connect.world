62525b0d803fe6bcb70a14a90fa25958c98c7885
connectoniccom <Connectonic.com@gmail.com>
2025-06-06 18:28:55 +0300

    Update App.js

--- Src/Pages/App.js
+++ Src/Pages/App.js
@@ -1,1 +1,19 @@
+import Header from './components/Header';
+import Counter from './components/Counter';
+import UserList from './components/UserList';
+import ContactForm from './components/ContactForm';
+import ToggleMessage from './components/ToggleMessage';
+import './styles/App.css';
 
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Counter />
+      <UserList />
+      <ContactForm />
+      <ToggleMessage />
+    </div>
+  );
+}
+export default App;
