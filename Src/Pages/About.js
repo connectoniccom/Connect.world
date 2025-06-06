877b37624f16f45da3150a729cc64cc181b726b0
connectoniccom <Connectonic.com@gmail.com>
2025-06-06 18:32:44 +0300

    Update About.js

--- Src/Pages/About.js
+++ Src/Pages/About.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './styles/About.css';
 
+function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>Built with React!</p>
+    </div>
+  );
+}
+export default About;
