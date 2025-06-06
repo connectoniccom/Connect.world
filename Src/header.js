259d42ce2879d35e791135d01f3cf287eab3b6f1
connectoniccom <Connectonic.com@gmail.com>
2025-06-06 18:30:05 +0300

    Update header.js

--- Src/header.js
+++ Src/header.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './styles/Header.css';
 
+function Header() {
+  return (
+    <header className="header">
+      <h1>My Website</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
