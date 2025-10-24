bbf2b255d25d136fbebbeddf813963ebee57c173
Firebase Studio <noreply@firebase.studio>
2025-10-24 15:56:35 +0000

    Initialized workspace with Firebase Studio

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+
+    firebase: {
+      apiKey: "AIzaSyBjSE2Hjv9t3BvYOaQBTQCt8jOMh333op0",
+      authDomain: "connectonic-live.firebaseapp.com",
+      projectId: "connectonic-live",
+      storageBucket: "connectonic-live.firebasestorage.app",
+      messagingSenderId: "1008276007753",
+      appId: "1:1008276007753:web:1016aacb1c2617590ec898",
+    }
+    };
